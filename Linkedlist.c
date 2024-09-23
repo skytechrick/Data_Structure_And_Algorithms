765c37971f7ffc0434361e3d74af908c20ad9a1d
= <ricksarkar2005@gmail.com>
2024-09-24 02:05:26 +0530

    Single linkedlist with single pointer - Linked List

---
+++ Linkedlist.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+typedef struct ll {
+    int data;
+    struct ll* next;
+} Node;
+
+
+
+
+int main() {
+    Node* head = NULL;
+    printf("%d", head);
+
+
+
+
+
+    return 0;
+}
---
+++ Linkedlist_Single_Pointer.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct ll {
+    int data;
+    struct ll* next;
+} Node;
+
+
+Node *Insert(Node *head , int data){
+    Node *New_Node = (Node*)malloc(sizeof(Node));
+    New_Node->data = data;
+    New_Node->next = NULL;
+
+    if (head == NULL)
+    {
+        return New_Node;
+    }
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = New_Node;
+    return head;
+}
+
+void Traverse (Node *head){
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    };
+    printf("NULL\n");
+};
+
+int main()
+{
+    Node *head = NULL;
+    int choice, a = 1, data;
+    while(a==1){
+        printf(" \n1. Enter data to add ..: \n2. Traverse linked list..: \n0. Exit..:\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+                printf("Enter data: ");
+                scanf("%d", &data);
+                head = Insert(head, data);
+                break;
+            case 2:
+                Traverse(head);
+                break;
+            case 0:
+                a = 0;
+                break;
+            default:
+                printf("Invalid choice\n");
+                break;
+        };
+    };
+    return 0;
+}
