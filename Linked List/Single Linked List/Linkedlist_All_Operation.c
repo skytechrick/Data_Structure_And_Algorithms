5f94ecf324d49181ddce948644c0e9af11ffdb9b
= <ricksarkar2005@gmail.com>
2024-09-24 18:33:01 +0530

    Linked list - Delete data from Linked list - Linked List.

--- Linked List/Single Linked List/Linkedlist_All_Operation.c
+++ Linked List/Single Linked List/Linkedlist_All_Operation.c
@@ -51,8 +51,43 @@ void Searching(Node *head, int data){
     };
     printf("Data not found.");
 }
-int main ()
-{
+void Count(Node *head){
+    int c=0;
+    Node *temp = head;
+    while(temp != NULL){
+        c++;
+        temp= temp->next;
+    };
+    printf("Total number of elements are..: %d", c);  
+};
+
+void Delete (Node **head, int del){
+    Node *temp = *head;
+    if(temp == NULL){
+        printf("Linked list is empty.");
+        return;
+    };
+    if(temp != NULL && temp->data == del){
+        *head = temp->next;
+        free(temp);
+        printf("Deleted successfully.");
+        return;
+    };
+    Node *prev = NULL;
+    while (temp != NULL){
+        if(temp->data == del){
+            prev->next = temp->next;
+            free(temp);
+            printf("Deleted successfully.");
+            return;
+        };
+        prev = temp;
+        temp = temp->next;
+    };
+    printf("Data not found for deletion.");
+    
+};
+int main (){
     Node *head = NULL;
     int a = 1, ch, data;
     while (a == 1){
@@ -71,6 +106,9 @@ int main ()
                 InsertEnd(&head, data);
                 break;
             case 3: 
+                printf("Enter a data to delete..: ");
+                scanf("%d", &data);
+                Delete(&head, data);
                 break;
             case 4: 
                 Traverse(head);
@@ -79,6 +117,9 @@ int main ()
                 printf("Enter a data Search..: ");
                 scanf("%d", &data);
                 Searching(head, data);
+                break;
+            case 6:
+                Count(head);
                 break;
             case 0:
                 a = 3; 
