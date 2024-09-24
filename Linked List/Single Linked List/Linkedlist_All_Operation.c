a9a9d05fc951f0eff6cd8fd2bfde00d789e2ec1b
= <ricksarkar2005@gmail.com>
2024-09-24 17:24:50 +0530

    Linked list - Searching added successfully - Linked List.

--- Linked List/Single Linked List/Linkedlist_All_Operation.c
+++ Linked List/Single Linked List/Linkedlist_All_Operation.c
@@ -4,7 +4,6 @@ typedef struct ll {
     int data;
     struct ll *next;
 } Node;
-
 void InsertBeg(Node **head, int data){
     Node *New_Node = NULL;
     New_Node = (Node*)malloc(sizeof(Node));
@@ -18,7 +17,6 @@ void InsertBeg(Node **head, int data){
     New_Node->next = *head;
     *head = New_Node;
 };
-
 void InsertEnd(Node **head,int data){
     Node *New_Node = (Node*)malloc(sizeof(Node));
     New_Node->data = data;
@@ -26,33 +24,39 @@ void InsertEnd(Node **head,int data){
     if (*head == NULL){
         *head = New_Node;
         return;
-    }
+    };
     Node *temp = *head;
     while(temp->next != NULL){
         temp = temp->next;
-    }
+    };
     temp->next = New_Node;
 };
 void Traverse(Node *head){
     Node *temp = head;
-    while (temp != NULL)
-    {   
+    while (temp != NULL){   
         printf("%d -> ", temp->data);
         temp=temp->next;
-    }
+    };
     printf("NULL");
     
 }
-
-
-
-
+void Searching(Node *head, int data){
+    Node *temp = head;
+    while (temp != NULL){
+        if (temp->data == data){
+            printf("Data found");
+            return;
+        };
+        temp= temp->next;
+    };
+    printf("Data not found.");
+}
 int main ()
 {
     Node *head = NULL;
     int a = 1, ch, data;
     while (a == 1){
-        printf("\n1. Add a number at beggining \n2. Add a number at the end \n3. Delete a number\n4. Traverse\n5. Searching \n0. Exit\nChoose a operation..: ");
+        printf("\n1. Add a number at beggining \n2. Add a number at the end \n3. Delete a number\n4. Traverse\n5. Searching \n6. Count \n0. Exit\nChoose a operation..: ");
         scanf("%d",&ch);
         switch (ch)
         {
@@ -71,7 +75,10 @@ int main ()
             case 4: 
                 Traverse(head);
                 break;
-            case 5: 
+            case 5:
+                printf("Enter a data Search..: ");
+                scanf("%d", &data);
+                Searching(head, data);
                 break;
             case 0:
                 a = 3; 
