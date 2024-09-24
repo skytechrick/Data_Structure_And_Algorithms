a2ea999d20abf31340661b3f8d541f79d1fc9088
= <ricksarkar2005@gmail.com>
2024-09-24 23:40:13 +0530

    File name changed

--- Linked List/Double Linked List/Double_Linkedlist_Single_Pointer.c
+++ Linked List/Double Linked List/Double_Linkedlist_Single_Pointer.c
@@ -8,28 +8,21 @@ typedef struct dll {
 
 Node *Create(Node *head,int data){
     Node *temp = head;
-    if(temp == NULL){
-        Node *New_Node = NULL;
-        New_Node = (Node*)malloc(sizeof(Node));
-        New_Node->prev = NULL;
-        New_Node->data = data;
-        New_Node->next = NULL;
-        return New_Node;
-    };
     Node *New_Node = NULL;
     New_Node = (Node*)malloc(sizeof(Node));
     New_Node->data = data;
     New_Node->next = NULL;
-
+    if(temp == NULL){
+        New_Node->prev = NULL;
+        return New_Node;
+    };
     while(temp->next != NULL){
         temp = temp->next;
     };
     New_Node->prev = temp;
     temp->next = New_Node;
-    return head;
-    
+    return head;  
 };
-
 void traverse(Node *head){
     Node *temp = head;
     if(temp == NULL){
@@ -40,9 +33,7 @@ void traverse(Node *head){
         printf("%d -> ", temp->data);
         temp = temp->next;
     };
-
     printf("NULL\n");
-    
 }
 void reverse(Node *head){
     Node *temp = head;
@@ -56,21 +47,17 @@ void reverse(Node *head){
     while(temp != NULL){
         printf("%d -> ", temp->data);
         temp = temp->prev;
-    }
+    };
     printf("NULL");
 };
-
 int main(){
     Node *head = NULL;
-
-
     head = Create(head,11);
     head = Create(head,22);
     head = Create(head,33);
     head = Create(head,44);
-
+    head = Create(head,55);
     traverse(head);
     reverse(head);
-
     return 0;
 }
