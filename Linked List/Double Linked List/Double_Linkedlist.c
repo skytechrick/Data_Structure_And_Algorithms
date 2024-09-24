1909cb53a7d78123abaa9ddd5e48825599fac3ca
= <ricksarkar2005@gmail.com>
2024-09-24 23:37:01 +0530

    Double linked list creation traverse and reverse done. Now next I am going to write a menu driven program in Main function.

---
+++ Linked List/Double Linked List/Double_Linkedlist.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct dll {
+    struct dll *prev;
+    int data;
+    struct dll *next;
+}Node;
+
+Node *Create(Node *head,int data){
+    Node *temp = head;
+    if(temp == NULL){
+        Node *New_Node = NULL;
+        New_Node = (Node*)malloc(sizeof(Node));
+        New_Node->prev = NULL;
+        New_Node->data = data;
+        New_Node->next = NULL;
+        return New_Node;
+    };
+    Node *New_Node = NULL;
+    New_Node = (Node*)malloc(sizeof(Node));
+    New_Node->data = data;
+    New_Node->next = NULL;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    };
+    New_Node->prev = temp;
+    temp->next = New_Node;
+    return head;
+    
+};
+
+void traverse(Node *head){
+    Node *temp = head;
+    if(temp == NULL){
+        printf("Double linked list is empty.");
+        return;
+    };
+    while(temp != NULL){
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    };
+
+    printf("NULL\n");
+    
+}
+void reverse(Node *head){
+    Node *temp = head;
+    if(temp == NULL){
+        printf("Double linked list is empty.");
+        return;
+    };
+    while(temp->next != NULL){
+        temp = temp->next;
+    };
+    while(temp != NULL){
+        printf("%d -> ", temp->data);
+        temp = temp->prev;
+    }
+    printf("NULL");
+};
+
+int main(){
+    Node *head = NULL;
+
+
+    head = Create(head,11);
+    head = Create(head,22);
+    head = Create(head,33);
+    head = Create(head,44);
+
+    traverse(head);
+    reverse(head);
+
+    return 0;
+}
