5113d4cb769d9ee48cdc5c6212d44ac88c4a2f62
= <ricksarkar2005@gmail.com>
2024-09-26 00:03:35 +0530

    All operation on double linked list is done.

--- Linked List/Double Linked List/Linkedlist_All_Operation.c
+++ Linked List/Double Linked List/Linkedlist_All_Operation.c
@@ -39,7 +39,7 @@ void InsertEnd(Node **head, int data){
 void Traverse(Node *head){
     Node *temp = head;
     if(temp == NULL){
-        printf("Double linked list is empty.");
+        printf("Double linked list is empty.\n");
         return;
     };
     while(temp != NULL){
@@ -51,7 +51,7 @@ void Traverse(Node *head){
 void Reverse(Node *head){
     Node *temp = head;
     if(temp == NULL){
-        printf("Double linked list is empty.");
+        printf("Double linked list is empty.\n");
         return;
     };
     while(temp->next != NULL){
@@ -63,11 +63,75 @@ void Reverse(Node *head){
     }; 
     printf("NULL\n");
 };
+
+void Search(Node *head, int val){
+    Node *temp = head;
+    while(temp != NULL){
+        if(temp->data == val){
+            printf("Data found.\n");
+            return;
+        };
+        temp = temp->next;
+    };
+    printf("No data found.\n");
+    
+}
+
+void Count(Node *head){
+    Node *temp = head;
+    int c = 0;
+    while(temp != NULL){
+        c++;
+        temp = temp->next;
+    };
+    printf("Number of elements = %d\n", c);
+    
+}
+void Delete(Node **head,int del){
+    Node *temp = *head;
+    if(temp == NULL){
+        printf("Double linked list is empty.\n");
+        return;
+    };
+
+    if(temp ->next == NULL && temp->data == del){
+        *head = NULL;
+        free(temp);
+        printf("Data deleted successfully.\n");
+        return;
+    };
+
+    Node *ptr = NULL;
+    while(temp != NULL){
+        if(temp->data == del){
+            if(ptr == NULL){
+                *head = temp->next;
+                free(temp);
+                printf("Data deleted successfully.\n");
+                return;
+            };
+            if(temp->next == NULL){
+                ptr->next = NULL;
+                free(temp);
+                printf("Data deleted successfully.\n");
+                return;
+            };
+            ptr->next = temp->next;
+            temp->next->prev = ptr;
+            free(temp);
+            printf("Data deleted successfully.\n");
+            return;
+        };
+        ptr = temp;
+        temp = temp->next;
+    };
+    printf("Data not present.\n");
+};
 int main(){
     Node *head = NULL;
     int a = 1,ch, data;
     while(a == 1){
-        printf("1. Add data to end\n2. Add data to beg\n3. Traverse\n4. Delete\n5. Search\n6. Count\n7. Reverse \n0. Exit\nChoose a number...:");
+        printf("\n1. Add data to end\n2. Add data to beg\n3. Traverse\n4. Delete\n5. Search\n6. Count\n7. Reverse \n0. Exit\nChoose a number...:");
         scanf("%d",&ch);
         switch(ch){
             case 1:
@@ -82,6 +146,19 @@ int main(){
                 break;
             case 3:
                 Traverse(head);
+                break;
+            case 5:
+                printf("Enter data to search..:");
+                scanf("%d",&data);
+                Search(head, data);
+                break;
+            case 4:
+                printf("Enter data to delete..:");
+                scanf("%d",&data);
+                Delete(&head, data);
+                break;
+            case 6:
+                Count(head);
                 break;
             case 7:
                 Reverse(head);
